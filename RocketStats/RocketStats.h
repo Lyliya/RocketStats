009b70027701f2bb232eeb9a5a2c5de7ced1fab2
Larsluph <remarso59@sfr.fr>
2022-02-11 22:53:08 +0100

    Revert "Complete plugin structure rewrite"
    This reverts commit 6b5b5d0c8bd5b8585447823608cd855c06fa485d.

--- RocketStats/RocketStats.h
+++ RocketStats/RocketStats.h
@@ -3,118 +3,120 @@
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 
-#include "Structs.h"
+struct Stats {
+	float myMMR = 100.0f;
+	float MMRChange = 0.0f;
+	int win = 0;
+	int losses = 0;
+	int streak = 0;
+	bool isInit = 0;
+};
 
-#define replaceAll(str, from, to) while(replace(str, from, to));
+class RocketStats : public BakkesMod::Plugin::BakkesModPlugin
+{
+private:
+	std::shared_ptr<bool> enabled;
 
-namespace NS_RS {
-	constexpr auto plugin_version = "3.5";
+public:
+	virtual void onLoad();
+	virtual void onUnload();
 
-	class RocketStats : public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginWindow*/
-	{
-		int currentPlaylist = 0;
-		bool isGameEnded = false;
-		bool isGameStarted = false;
-		bool isBoosting = false;
+	void LogImageLoadStatus(bool status, std::string imageName);
+	std::string GetRank(int tierID);
+	std::string GetPlaylistName(int playlistID);
+	void replaceAll(std::string& str, const std::string& from, const std::string& to);
+	void LoadImgs();
 
-		std::map<int, Stats> stats;
-		Stats session;
+	void GameStart(std::string eventName);
+	void GameEnd(std::string eventName);
+	void GameDestroyed(std::string eventName);
 
-		//std::unique_ptr<MMRNotifierToken> notifierToken;
+	void UpdateMMR(UniqueIDWrapper id);
+	void SessionStats();
+	void ResetStats();
 
-		int myTeamNum = -1;
-
-		int lastGameMode = 0;
-		int currentGameMode = 0;
-		float currentMMR = 100.0f;
-		int currentTier = 0;
-
-		std::string currentDivision;
-		std::string currentRank;
-		std::string lastRank;
-
-		std::shared_ptr<ImageWrapper> crown;
-		std::shared_ptr<ImageWrapper> win;
-		std::shared_ptr<ImageWrapper> loose;
-		std::shared_ptr<ImageWrapper> streak;
-
-		int rank_nb = 23;
-
-		//Plugin Load Routines
-		virtual void onLoad();
-		virtual void onUnload();
-
-		// Utils
-		std::string GetRank(int tierID);
-		std::string GetPlaylistName(int playlistID);
-		void LoadImgs();
-		void LogImageLoadStatus(bool status, std::string imageName);
-
-		// Game Hooks
-		void GameStart(std::string eventName);
-		void GameEnd(std::string eventName);
-		void GameDestroyed(std::string eventName);
-
-		// Stats mgmt
-		void UpdateMMR(UniqueIDWrapper id);
-		void SessionStats();
-		void ResetStats();
-
-		// Boost Hooks
-		void OnBoostStart(std::string eventName);
-		void OnBoostEnd(std::string eventName);
-
-		// Rank / Div
-		void InitRank();
-		void MajRank(int _gameMode, bool isRanked, float _currentMMR, SkillRank playerRank);
-
-		// Overlay mgmt
-		void DisplayRank(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, float scale, bool showText);
-		void DisplayMMR(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale, bool showImage);
-		void DisplayWins(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale);
-		void DisplayLoose(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale);
-		void DisplayStreak(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale, bool showImage);
-
-		// File I/O
-		void WriteInFile(std::string _fileName, std::string _value);
-		void writeGameMode();
-		void writeMMR();
-		void writeMMRChange();
-		void writeStreak();
-		void writeWin();
-		void writeLosses();
-
-		// Render
-		void Render(CanvasWrapper canvas);
+	void OnBoostStart(std::string eventName);
+	void OnBoostEnd(std::string eventName);
+	//void StopBoost();
 
 
-		t_ranks rank[23] = {
-			{"Unranked", nullptr},
-			{"Bronze_I", nullptr},
-			{"Bronze_II", nullptr},
-			{"Bronze_III", nullptr},
-			{"Silver_I", nullptr},
-			{"Silver_II", nullptr},
-			{"Silver_III", nullptr},
-			{"Gold_I", nullptr},
-			{"Gold_II", nullptr},
-			{"Gold_III", nullptr},
-			{"Platinum_I", nullptr},
-			{"Platinum_II", nullptr},
-			{"Platinum_III", nullptr},
-			{"Diamond_I", nullptr},
-			{"Diamond_II", nullptr},
-			{"Diamond_III", nullptr},
-			{"Champion_I", nullptr},
-			{"Champion_II", nullptr},
-			{"Champion_III", nullptr},
-			{"Grand_Champion_I", nullptr},
-			{"Grand_Champion_II", nullptr},
-			{"Grand_Champion_III", nullptr},
-			{"Supersonic_Legend", nullptr},
-		};
+	void InitRank();
+	void MajRank(int _gameMode, bool isRanked, float _currentMMR, SkillRank playerRank);
 
-		const std::map<int, std::string> playlistName = {
+	void DisplayRank(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, float scale, bool showText);
+	void DisplayMMR(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale, bool showImage);
+	void DisplayWins(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale);
+	void DisplayLoose(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale);
+	void DisplayStreak(CanvasWrapper canvas, Vector2 imagePos, Vector2 textPos_tmp, Stats current, float scale, bool showImage);
+	void Render(CanvasWrapper canvas);
+
+	void WriteInFile(std::string _fileName, std::string _value);
+	void writeGameMode();
+	void writeMMR();
+	void writeMMRChange();
+	void writeStreak();
+	void writeWin();
+	void writeLosses();
+
+	int currentPlaylist = 0;
+	bool isGameEnded = false;
+	bool isGameStarted = false;
+	bool isBoosting = false;
+
+	std::map<int, Stats> stats;
+	Stats session;
+
+	//std::unique_ptr<MMRNotifierToken> notifierToken;
+
+	int myTeamNum = -1;
+
+	int lastGameMode = 0;
+	int currentGameMode = 0;
+	float currentMMR = 100.0f;
+	int currentTier = 0;
+	std::string currentDivision;
+	std::string currentRank;
+	std::string lastRank;
+
+	std::shared_ptr<ImageWrapper> crown;
+	std::shared_ptr<ImageWrapper> win;
+	std::shared_ptr<ImageWrapper> loose;
+	std::shared_ptr<ImageWrapper> streak;
+
+	int rank_nb = 23;
+
+	typedef struct s_ranks {
+		std::string name;
+		std::shared_ptr<ImageWrapper> image;
+	} t_ranks;
+
+	t_ranks rank[23] = {
+		{"Unranked", nullptr},
+		{"Bronze_I", nullptr},
+		{"Bronze_II", nullptr},
+		{"Bronze_III", nullptr},
+		{"Silver_I", nullptr},
+		{"Silver_II", nullptr},
+		{"Silver_III", nullptr},
+		{"Gold_I", nullptr},
+		{"Gold_II", nullptr},
+		{"Gold_III", nullptr},
+		{"Platinum_I", nullptr},
+		{"Platinum_II", nullptr},
+		{"Platinum_III", nullptr},
+		{"Diamond_I", nullptr},
+		{"Diamond_II", nullptr},
+		{"Diamond_III", nullptr},
+		{"Champion_I", nullptr},
+		{"Champion_II", nullptr},
+		{"Champion_III", nullptr},
+		{"Grand_Champion_I", nullptr},
+		{"Grand_Champion_II", nullptr},
+		{"Grand_Champion_III", nullptr},
+		{"Supersonic_Legend", nullptr},
+	};
+
+	const std::map<int, std::string> playlistName = {
 		{1, "Duel"},
 		{2, "Doubles"},
 		{3, "Standard"},
@@ -162,6 +164,5 @@ namespace NS_RS {
 		{47, "Super Cube"},
 		{48, "Tactical Rumble"},
 		{49, "Spring Loaded"}
-		};
 	};
-}
+};
